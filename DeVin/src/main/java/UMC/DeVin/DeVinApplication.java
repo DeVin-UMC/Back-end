a2583a3a8d88034ff9bc63fce7019165d499f220
CHO Min HO <chominho96@naver.com>
2022-07-05 15:32:38 +0900

    [기능추가] 프로젝트 파일 생성
    1. Spring 프로젝트 파일 생성
    2. build.gradle 수정

---
+++ DeVin/src/main/java/UMC/DeVin/DeVinApplication.java
@@ -0,0 +1,13 @@
+package UMC.DeVin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeVinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeVinApplication.class, args);
+	}
+
+}
