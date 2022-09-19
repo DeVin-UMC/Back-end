36b9d1cceed216fc4fbed8c154744bad55ec9c9f
Min Ho CHO <chominho96@naver.com>
2022-09-19 15:43:28 +0900

    [버그] contextLoads() 제거

--- src/test/java/UMC/DeVin/DeVinApplicationTests.java
+++ src/test/java/UMC/DeVin/DeVinApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DeVinApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
