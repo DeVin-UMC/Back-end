8c18819cd4fa64fa6fceed25b7e0b2b51c035269
Min Ho CHO <chominho96@naver.com>
2022-11-09 22:34:57 +0900

    [기능수정] DTO 클래스에 @AllArgsConstructor 추가
    테스트 데이터 추가를 위한 @AllArgsConstructor 애노테이션을 DTO 클래스들에 추가

--- src/main/java/UMC/DeVin/qna/dto/PostTagReq.java
+++ src/main/java/UMC/DeVin/qna/dto/PostTagReq.java
@@ -1,10 +1,12 @@
 package UMC.DeVin.qna.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class PostTagReq {
     private String title;
 }
