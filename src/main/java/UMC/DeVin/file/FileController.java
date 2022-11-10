4069d218997764c304b3fb7c9a0f524e3e844191
Min Ho CHO <chominho96@naver.com>
2022-11-10 17:15:03 +0900

    [주석] 파일 업로드 관련 클래스 주석 추가

--- src/main/java/UMC/DeVin/file/FileController.java
+++ src/main/java/UMC/DeVin/file/FileController.java
@@ -17,6 +17,14 @@ public class FileController {
 
     private final FileUploadUtil fileUploadUtil;
 
+    /**
+     * 파일 업로드 테스트 컨트롤러입니다.
+     * 카테고리와 파일을 입력으로 받고, 파일 업로드를 수행합니다.
+     * @param category 해당 파일의 분류입니다. category를 project로 할 경우 S3 상에서 project 폴더에 들어가게 됩니다.
+     * @param multipartFile 입력으로 들어오는 파일
+     * @return 생성된 파일 URL을 담은 DTO response 객체
+     * @throws BaseException 파일 업로드 실패 시 예외를 던집니다.
+     */
     @PostMapping("/test/upload")
     public BaseResponse<FileDTO> uploadFile(@RequestParam("category") String category,
                                             @RequestParam(value = "file")MultipartFile multipartFile) throws BaseException {
