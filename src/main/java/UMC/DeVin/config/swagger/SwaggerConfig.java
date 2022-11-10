b9e26ffc34af7efbbe3077ae2a1fd870aad4e524
zer0silver <duddms0_0@naver.com>
2022-11-10 22:25:34 +0900

    [기능추가] swagger 적용

--- src/main/java/UMC/DeVin/config/swagger/SwaggerConfig.java
+++ src/main/java/UMC/DeVin/config/swagger/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package UMC.DeVin.config;
+package UMC.DeVin.config.swagger;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
@@ -8,20 +8,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
-        info = @Info(title = "Devin API 명세서",
-                description = "개발 커뮤니티 서비스 API 명세서",
+        info = @Info(title = "DeVin API Specification",
+                description = "SW 분야 종합 커뮤니티 서비스 DeVin API 명세서",
                 version = "v1"))
 @RequiredArgsConstructor
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public GroupedOpenApi chatOpenApi() {
-        String[] paths = {"/"};
+    public GroupedOpenApi devinApi() {
+        String[] paths = {"/**"};
 
         return GroupedOpenApi.builder()
-                .group("API v1")
+                .group("DeVin API v1")
                 .pathsToMatch(paths)
                 .build();
     }
+
 }
