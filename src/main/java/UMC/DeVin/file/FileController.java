b738c2bce808e1e4ae3db7123eb54ac6d7991165
CHO Min HO <chominho96@naver.com>
2022-11-10 23:58:12 +0900

    [기능추가] 파일 업로드 테스트 API Swagger 추가
    - 파일 업로드 테스트 API Swagger 추가

--- src/main/java/UMC/DeVin/file/FileController.java
+++ src/main/java/UMC/DeVin/file/FileController.java
@@ -4,8 +4,15 @@ import UMC.DeVin.common.base.BaseException;
 import UMC.DeVin.common.base.BaseResponse;
 import UMC.DeVin.file.dto.DeleteFileDTO;
 import UMC.DeVin.file.dto.FileDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,9 +34,20 @@ public class FileController {
      * @return 생성된 파일 URL을 담은 DTO response 객체
      * @throws BaseException 파일 업로드 실패 시 예외를 던집니다.
      */
-    @PostMapping("/test/file/upload")
-    public BaseResponse<FileDTO> uploadFile(@RequestParam("category") String category,
-                                            @RequestParam(value = "file")MultipartFile multipartFile) throws BaseException {
+    @Operation(summary = "파일 업로드 테스트 API", description = "AWS S3 파일 업로드 테스트 API입니다.", tags = { "Test File Controller" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(schema = @Schema(implementation = FileDTO.class))),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST - 빈 파일이거나, 파일의 크기가 20MB를 넘어가는 경우",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR - 서버에서 파일 저장에 실패한 경우",
+                    content = @Content)
+    })
+    @PostMapping(value = "/test/file/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public BaseResponse<FileDTO> uploadFile(
+            @Parameter(name = "category", description = "파일의 유형 (project, study, qna)") @RequestParam("category") String category,
+            @Parameter(name = "file", description = "업로드할 파일") @RequestParam(value = "file") MultipartFile multipartFile)
+            throws BaseException {
 
         return new BaseResponse<>(new FileDTO(fileUploadUtil.uploadFileV1(category, multipartFile)));
     }
